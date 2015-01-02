698c2007cf6e07735cc6119a1df77b62e7cd490b
rbfriedman <renabf@gmail.cmom>
2015-01-02 12:23:58 -0500

    TaskFactoryType

--- src/guessGame/TaskFactoryType.java
+++ src/guessGame/TaskFactoryType.java
@@ -1,9 +1,16 @@
 package guessGame;
 
 public enum TaskFactoryType {
-	BINARY("application/octet-stream"), JPEG("image/jpeg");
-	public String decsription;
+	/*Taken form 
+	 * http://www.freeformatter.com/mime-types-list.html
+	 */
+	BINARY("application/octet-stream"), JPEG("image/jpeg"), GIF("image/gif"),TEXT("text/plain");
+	public String description;
 	TaskFactoryType(String description){
-		this.decsription = description;
+		this.description = description;
+	}
+	@Override
+	public String toString(){
+		return description;
 	}
 }
